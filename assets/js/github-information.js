5ee39b0e7d9dfddd4659170b2fca8870d3994b9a
Sergii Kostanets <sergiikostanets@gmail.com>
2023-04-10 16:26:04 +0100

    fix bugs with empying gh divs

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -54,6 +54,9 @@ function repoInformationHTML(repos){
 }
 
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo -data").html("");
+    
     var username = $("#gh-username").val();
     
     if (!username){
@@ -89,3 +92,5 @@ function fetchGitHubInformation(event) {
         }
     );
 }
+
+$(document).ready(fetchGitHubInformation);
